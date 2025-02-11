d75c1e5ea959fd548441dacac8063bdb69e8dc0e
Leggendarii <nidar@dtu.dk>
2025-02-11 12:30:16 +0100

    reordering the main script

--- Prueba_Interaction_Assessment.m
+++ Prueba_Interaction_Assessment.m
@@ -1,47 +1,50 @@
 clc
 clear
-% Load data from FD scan
+
+%% Load data from FD scan
 load('Yqd_grid_2.mat');
 load('Yqd_grid_nostable.mat');
 load('Yqd_GFL_100f.mat');
 
-% Performing "det" and "eig" operations
-Y_qq_g = zeros(1,length(Yqq));
-Y_qd_g = zeros(1,length(Yqd));
-Y_dq_g = zeros(1,length(Ydq));
-Y_dd_g = zeros(1,length(Ydd));
+%% Setup impedance/Ammetance matrix and compute eigenvalues
 
 for n=1:length(fd0)
+    % Grid Matrixes
     Zgrid_stable=inv([Yqq_g(n), Yqd_g(n); 
                       Ydq_g(n), Ydd_g(n)]);
-    Y_qq_g(n) = Yqq_g(n);
-    Y_qd_g(n) = Yqd_g(n);
-    Y_dq_g(n) = Ydq_g(n);
-    Y_dd_g(n) = Ydd_g(n);
+
+    Ygrid_stable=([Yqq_g(n), Yqd_g(n); 
+                      Ydq_g(n), Ydd_g(n)]);
 
     Zgrid_unstable=inv([Yqq_g_ns(n), Yqd_g_ns(n); 
                         Ydq_g_ns(n), Ydd_g_ns(n)]);
+
+    Ygrid_unstable=([Yqq_g_ns(n), Yqd_g_ns(n); 
+                      Ydq_g_ns(n), Ydd_g_ns(n)]);
+
+    % GFL matrix
     Y_GFL=[Yqq(n) Yqd(n) 
            Ydq(n) Ydd(n)];
 
-    
-    % % Matlab suggested GNC evaluation
+    % Open loop gain per each frequency stable case
     L_1(:,:,n)=Zgrid_stable*Y_GFL;
+    % Open loop gain per each frequency unstable case
     L_2(:,:,n)=Zgrid_unstable*Y_GFL;
+
+    % Eigenvalues computation per each frequency
+    E_GFL(:,n) = eig(Y_GFL);
+    E_grid(:,n) = eig(Ygrid_stable);
+    E_loop_stable(:,n) = eig(L_1(:,:,n));
+    E_loop_unstable(:,n) = eig(L_2(:,:,n));
+
 end
 
-%% Test
-GFL = Yqd;
-Grid = Y_qd_g;
-name = "qd"
-
-bode_plot_single(fd0,Yqd,Y_qd_g,name)
-
+%% Work area
 % 1: lui calcola gli eignevalues del loop gain per ogni frequenza prima
 % 2: sorting degli eig
 % 3: N= Z-P del denominatore: N>0 -> Closed loop unstable, N<0-> Loop Gain
 % unstable, N=0 -> Stable
+% 4: il bro conta i giri attorno a -1 pero
 
-
-% RHP_poles = sum(real(pole(Hsys)) > 0)
-% plot_eigenvalues_in_sequence(-1000:0.1:1000, E);
+bode_plot_single(fd0,Yqq,Yqq_g,"qq")
+plot_eigenvalues_in_sequence(10:102, E_loop_unstable(:,10:102));
