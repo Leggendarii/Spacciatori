5ba2524f1965e09a75903653fb173523d2f97b2c
Leggendarii <nidar@dtu.dk>
2025-03-06 16:57:27 +0100

    Update Multi Case Plots

--- bode_plot_full.m
+++ bode_plot_full.m
@@ -1,18 +1,44 @@
-function bode_plot_full(fd0, Y_c, Y_g)
+function bode_plot_full(fd0, Y_cs, Y_gs)
 
-Ydd_g = squeeze(Y_g(1,1,:));
-Ydq_g = squeeze(Y_g(1,2,:));
-Yqd_g = squeeze(Y_g(2,1,:));
-Yqq_g = squeeze(Y_g(2,2,:));
+Ydd_gs = {};
+Ydq_gs = {};
+Yqd_gs = {};
+Yqq_gs = {};
 
-Ydd_c = squeeze(Y_c(1,1,:));
-Ydq_c = squeeze(Y_c(1,2,:));
-Yqd_c = squeeze(Y_c(2,1,:));
-Yqq_c = squeeze(Y_c(2,2,:));
+Ydd_cs = {};
+Ydq_cs = {};
+Yqd_cs = {};
+Yqq_cs = {};
 
+for i=1:length(Y_gs)
+    Y_g = Y_gs{i};
+    Ydd_g = squeeze(Y_g(1,1,:));
+    Ydq_g = squeeze(Y_g(1,2,:));
+    Yqd_g = squeeze(Y_g(2,1,:));
+    Yqq_g = squeeze(Y_g(2,2,:));
 
-bode_plot_single(fd0,Ydd_c,Ydd_g,"dd")
-bode_plot_single(fd0,Ydq_c,Ydq_g,"dq")
-bode_plot_single(fd0,Yqd_c,Yqd_g,"qd")
-bode_plot_single(fd0,Yqq_c,Yqq_g,"qq")
+    Ydd_gs = appendCellRow(Ydd_gs, Ydd_g);
+    Ydq_gs = appendCellRow(Ydq_gs, Ydq_g);
+    Yqd_gs = appendCellRow(Yqd_gs, Yqd_g);
+    Yqq_gs = appendCellRow(Yqq_gs, Yqq_g);
 end
+
+for i=1:length(Y_cs)
+    Y_c = Y_cs{i};
+    Ydd_c = squeeze(Y_c(1,1,:));
+    Ydq_c = squeeze(Y_c(1,2,:));
+    Yqd_c = squeeze(Y_c(2,1,:));
+    Yqq_c = squeeze(Y_c(2,2,:));
+    
+    Ydd_cs = appendCellRow(Ydd_cs, Ydd_c);
+    Ydq_cs = appendCellRow(Ydq_cs, Ydq_c);
+    Yqd_cs = appendCellRow(Yqd_cs, Yqd_c);
+    Yqq_cs = appendCellRow(Yqq_cs, Yqq_c);
+    
+end   
+    bode_plot_single(fd0,Ydd_cs,Ydd_gs,"dd")
+    bode_plot_single(fd0,Ydq_cs,Ydq_gs,"dq")
+    bode_plot_single(fd0,Yqd_cs,Yqd_gs,"qd")
+    bode_plot_single(fd0,Yqq_cs,Yqq_gs,"qq")
+end
+
--- bode_plot_single.m
+++ bode_plot_single.m
@@ -1,15 +1,22 @@
 function bode_plot_single(x,A,B,name)
 fd0 = x;
-GLF = A;
-Grid = B;
+% GFL = A;
+% Grid = B;
 
 figure
 subplot(2,1,1)
-semilogx(fd0, 20*log10(abs(GLF)), 'DisplayName', 'Converter')
 hold on
-semilogx(fd0, 20*log10(abs(Grid)), 'DisplayName', 'Grid')
+for i=1:length(A)
+GFL = A{i};
+semilogx(fd0, 20*log10(abs(GFL)), 'DisplayName', sprintf('GFL(%d)',i))
+end
+for i=1:length(B)
+Grid = B{i};
+semilogx(fd0, 20*log10(abs(Grid)), 'DisplayName', sprintf('Grid (%d)',i))
+end
 hold off
-xlim([0,1000])
+% xlim([0,1000])
+xlim auto,
 ylabel("Magnitude dB")
 xlabel("Frequency Hz")
 legend
@@ -17,11 +24,18 @@ grid on
 title(name + " magnitude")
 
 subplot(2,1,2)
-semilogx(fd0, angle(GLF), 'DisplayName', 'Converter')
 hold on
-semilogx(fd0, angle(Grid), 'DisplayName', 'Grid')
+for i=1:length(A)
+GFL = A{i};
+semilogx(fd0, angle(GFL), 'DisplayName', sprintf('GFL(%d)',i))
+end
+for i=1:length(B)
+Grid = B{i};
+semilogx(fd0, angle(Grid), 'DisplayName', sprintf('Grid (%d)',i))
+end
 hold off
-xlim([0,1000])
+% xlim([0,1000])
+xlim auto;
 ylabel("Angle rad")
 xlabel("Frequency Hz")
 legend
